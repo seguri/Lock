38a14c9d178ae45044b8fd34fdf05fe1724eed0e
Marco Seguri <seguri@users.noreply.github.com>
2018-04-17 12:45:42 +0200

    Fix string format in log

--- app/src/main/java/name/seguri/android/lock/MainActivity.java
+++ app/src/main/java/name/seguri/android/lock/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        i("onActivityResult: requestCode=%d resultCode=%d resultCodeString=", requestCode, resultCode, getResultString(resultCode));
+        i("onActivityResult: requestCode=%d resultCode=%d resultCodeString=%s", requestCode, resultCode, getResultString(resultCode));
         if (requestCode == REQUEST_CODE_ENABLE_ADMIN && resultCode == RESULT_OK) {
             lock();
         }
